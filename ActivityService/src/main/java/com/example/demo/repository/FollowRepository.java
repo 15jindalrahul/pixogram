1655ff0618dfefd01a737c16201e824c8c6ca793
15jindalrahul <15jindalrahul@gmail.com>
2020-03-12 07:00:18 -0700

    commit
    commit

---
+++ ActivityService/src/main/java/com/example/demo/repository/FollowRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Follow;
+@Repository
+public interface FollowRepository extends CrudRepository<Follow, Integer> {
+List<Follow> getByMyid(Integer myid);
+	
+	List<Follow> getByUserid(Integer userid);
+}
